ea44c963308114dd8a4954b0b48a869b942a4d08
AbrhamDemsew <138160056+AbrhamDemsew@users.noreply.github.com>
2024-07-28 14:32:47 +0300

    Add files via upload

--- Biometric-Attendance-and-HR-System_Frontend/client/src/App.js
+++ Biometric-Attendance-and-HR-System_Frontend/client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Password from './pages/Password';
+import OTPInput from './pages/OTPInput';
+import Recovered from './pages/Recovered';
+
+export const RecoveryContext = createContext();
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [otp, setOTP] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <RecoveryContext.Provider value={{ email, setEmail, otp, setOTP }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/pass" element={<Password />} />
+            <Route path="/otp" element={<OTPInput />} />
+            <Route path="/recovered" element={<Recovered />} />
+          </Routes>
+        </RecoveryContext.Provider>
+      </div>
+    </Router>
   );
 }
 
